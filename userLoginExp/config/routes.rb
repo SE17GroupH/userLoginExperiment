21bf479dfaf5542941cae8f567b40de226a97980
Harold Chen <ychen71@ncsu.edu>
2017-01-21 17:37:14 -0500

    Update homepage using bootstrap

--- userLoginExp/config/routes.rb
+++ userLoginExp/config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
-  get 'users/new'
-
-  root "users#index"
+  root "static_pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
